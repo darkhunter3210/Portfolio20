6bb724005327f23c3ff3091c6e31809aa22ea263
Matthew Cheung <matthewcheung3003@gmail.com>
2021-08-09 00:54:29 -0400

    added profile pic and arrow that move to the next part of website

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="asset/tanjiro.jpeg" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Matthew Cheung</h1>
+                    <h3>Software<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="asset/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
